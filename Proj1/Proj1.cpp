cd29fc5cb777e5c1d525a1117b215e8c62a55b20
JehadH1 <5088836@nwpolytech.ca>
2024-11-25 04:03:26 -0700

    Update Proj1.cpp

--- Proj1/Proj1.cpp
+++ Proj1/Proj1.cpp
@@ -4,21 +4,23 @@ using namespace std;
 
 long int clock_tick = 0;
 int main() {
-	initializeMemory();
-	srand(time(0));
+    initializeMemory();
+    srand(0);
 
-	do {
+    do {
 
 
-		cleanMemory(clock_tick);
-		if (clock_tick % REQUEST_INTERVAL == 0) {
-			size_t size = rand() % (MAX_SIZE - MIN_SIZE + 1) + MIN_SIZE;
-			size_t expiry = rand() % (MAX_LEASE - MIN_LEASE + 1) + MIN_LEASE;
-			cout << " size " << size << " " << "time " << expiry << " clock " << clock_tick;
-			cout << endl;
-			getMemory(size, expiry);
-			print();
-		}
-		clock_tick++;
-	} while (clock_tick != TIME_LIMIT);
+        cleanMemory(clock_tick);
+        if (clock_tick % REQUEST_INTERVAL == 0) {
+            size_t size = rand() % (MAX_SIZE - MIN_SIZE + 1) + MIN_SIZE;
+            size_t expiry = rand() % (MAX_LEASE - MIN_LEASE + 1) + MIN_LEASE;
+            cout <<" size "<< size << " " <<"time "<< expiry << " clock " << clock_tick;
+            cout << endl;
+            getMemory(size, expiry);
+            print();
+
+        }
+        clock_tick++;
+    } while (clock_tick != TIME_LIMIT);
+    printStats();
 }
